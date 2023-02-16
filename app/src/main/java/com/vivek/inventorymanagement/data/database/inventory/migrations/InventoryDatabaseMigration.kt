e264ea2092c7ac008283b2002eea8b3cef4cca37
Vivek Yadav <vivek.yadav@zestmoney.in>
2023-02-17 02:02:48 +0530

    removed redundant migration code

--- app/src/main/java/com/vivek/inventorymanagement/data/database/inventory/migrations/InventoryDatabaseMigration.kt
+++ app/src/main/java/com/vivek/inventorymanagement/data/database/inventory/migrations/InventoryDatabaseMigration.kt
@@ -4,13 +4,5 @@ import androidx.room.migration.Migration
 import androidx.sqlite.db.SupportSQLiteDatabase
 
 class InventoryDatabaseMigration {
-    companion object {
-        val MIGRATION_1_2 = object : Migration(1, 2) {
-            override fun migrate(database: SupportSQLiteDatabase) {
-                database.execSQL("ALTER TABLE item Add default 0 for id")
-            }
-        }
-    }
-
 
 }
