156427dba001f1c6014d5aabd6c5988449480d3c
v-zm <vivek.yadav@zestmoney.in>
2023-02-20 12:20:20 +0530

    moved constant url to InventoryApiConfig.kt

--- app/src/main/java/com/vivek/inventorymanagement/data/api/services/InventoryApiService.kt
+++ app/src/main/java/com/vivek/inventorymanagement/data/api/services/InventoryApiService.kt
@@ -1,11 +1,11 @@
 package com.vivek.inventorymanagement.data.api.services
 
-import com.vivek.inventorymanagement.data.api.dtos.InventoryItemDto
+import com.vivek.inventorymanagement.data.api.config.InventoryApiConfig
 import com.vivek.inventorymanagement.data.api.dtos.InventoryItemListDto
 import retrofit2.Call
 import retrofit2.http.GET
 
 public interface InventoryApiService {
-    @GET("v3/995ce2a0-1daf-4993-915f-8c198f3f752c")
+    @GET(InventoryApiConfig.GET_INVENTORY_ITEMS_URL)
     fun getInventoryList(): Call<InventoryItemListDto>
 }
