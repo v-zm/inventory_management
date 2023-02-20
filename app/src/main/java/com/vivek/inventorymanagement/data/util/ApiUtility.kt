66780263f7ce7e84540545894618c3e958f08e8c
v-zm <vivek.yadav@zestmoney.in>
2023-02-21 00:49:09 +0530

    reformatted ApiUtility.kt

--- app/src/main/java/com/vivek/inventorymanagement/data/util/ApiUtility.kt
+++ app/src/main/java/com/vivek/inventorymanagement/data/util/ApiUtility.kt
@@ -6,30 +6,30 @@ import android.net.NetworkInfo
 import android.os.Build
 
 class ApiUtility {
-companion object{
-    fun isInternetConnected(context: Context): Boolean {
-        val connectivityManager: ConnectivityManager =
-            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    companion object {
+        fun isInternetConnected(context: Context): Boolean {
+            val connectivityManager: ConnectivityManager =
+                context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
 
-        connectivityManager.let { tempConnectivityManager ->
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                val networkCapabilities =
-                    tempConnectivityManager.getNetworkCapabilities(tempConnectivityManager.activeNetwork)
-                if (networkCapabilities != null) {
-                    return true
+            connectivityManager.let { tempConnectivityManager ->
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    val networkCapabilities =
+                        tempConnectivityManager.getNetworkCapabilities(tempConnectivityManager.activeNetwork)
+                    if (networkCapabilities != null) {
+                        return true
+                    } else {
+                        //TODO::
+                    }
+
                 } else {
-                    //TODO::
-                }
-
-            } else {
-                val networkInfo: NetworkInfo? = connectivityManager.activeNetworkInfo
-                networkInfo?.let { tempNetworkInfo ->
-                    return tempNetworkInfo.isConnected
+                    val networkInfo: NetworkInfo? = connectivityManager.activeNetworkInfo
+                    networkInfo?.let { tempNetworkInfo ->
+                        return tempNetworkInfo.isConnected
+                    }
                 }
             }
-        }
-        return false
+            return false
 
+        }
     }
-}
 }
