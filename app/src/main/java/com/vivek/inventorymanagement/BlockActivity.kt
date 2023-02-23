7b5f585af4af099feffec9541741e34ef8f5b84e
v-zm <vivek.yadav@zestmoney.in>
2023-02-23 12:48:03 +0530

    added block activity

---
+++ app/src/main/java/com/vivek/inventorymanagement/BlockActivity.kt
@@ -0,0 +1,11 @@
+package com.vivek.inventorymanagement
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BlockActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_block)
+    }
+}
