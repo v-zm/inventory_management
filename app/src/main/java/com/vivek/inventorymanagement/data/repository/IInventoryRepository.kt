c2e50e5f67358b56768cab8f3d99fb238c40b416
v-zm <vivek.yadav@zestmoney.in>
2023-02-27 11:55:30 +0530

    removed unused functions from IInventoryRepository.kt

--- app/src/main/java/com/vivek/inventorymanagement/data/repository/IInventoryRepository.kt
+++ app/src/main/java/com/vivek/inventorymanagement/data/repository/IInventoryRepository.kt
@@ -2,9 +2,8 @@ package com.vivek.inventorymanagement.data.repository
 
 import com.vivek.inventorymanagement.features.inventory.enums.InventoryFilterOptionEnum
 import com.vivek.inventorymanagement.features.inventory.model.Item
-import com.vivek.inventorymanagement.features.inventory.viewstate.InventoryViewState
+import com.vivek.inventorymanagement.features.inventory.viewstate.InventoryItemFetchState
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.StateFlow
 
 interface IInventoryRepository {
     suspend fun getInventoryItems(): List<Item>?
@@ -13,11 +12,9 @@ interface IInventoryRepository {
         searchOnlyWithImage: Boolean = false
     ): List<Item>
 
-    fun getLatestQueriedItems(): StateFlow<InventoryViewState>
-    fun getLatestQueriedItems1(): StateFlow<List<Item>>
-
     fun inventorySearch(
-            searchText: String, searchType: InventoryFilterOptionEnum,
-        searchOnlyWithImage: Boolean = false
-    ): Flow<InventoryViewState>
+        searchText: String, searchType: InventoryFilterOptionEnum,
+        searchOnlyWithImage: Boolean = false,
+    ): Flow<InventoryItemFetchState>
+
 }
