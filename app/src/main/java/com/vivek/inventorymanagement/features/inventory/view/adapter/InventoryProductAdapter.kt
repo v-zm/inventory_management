9c9a47a8df8b31315ff58d5c18b15838d3e9d642
v-zm <vivek.yadav@zestmoney.in>
2023-02-27 14:42:36 +0530

    removed unwanted code from InventoryProductAdapter.kt

--- app/src/main/java/com/vivek/inventorymanagement/features/inventory/view/adapter/InventoryProductAdapter.kt
+++ app/src/main/java/com/vivek/inventorymanagement/features/inventory/view/adapter/InventoryProductAdapter.kt
@@ -1,14 +1,12 @@
 package com.vivek.inventorymanagement.features.inventory.view.adapter
 
-import android.net.Uri
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import coil.load
 import com.vivek.inventorymanagement.R
 import com.vivek.inventorymanagement.features.inventory.enums.ProductViewTypeEnum
 import com.vivek.inventorymanagement.features.inventory.model.Item
+import com.vivek.inventorymanagement.features.inventory.view.viewHolder.IViewHolder
 import com.vivek.inventorymanagement.features.inventory.view.viewHolder.ViewHolderGridView
 import com.vivek.inventorymanagement.features.inventory.view.viewHolder.ViewHolderListView
 
@@ -51,59 +49,10 @@ class InventoryProductAdapter(
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        when (mViewTypeEnum) {
-            ProductViewTypeEnum.LIST -> {
-                bindViewForViewHolderListView(
-                    holder,
-                    item = mItems[position],
-                    isLastItem = position == mItems.size - 1
-                )
-            }
-
-            ProductViewTypeEnum.GRID -> {
-                bindViewForViewHolderGridView(holder, item = mItems[position])
-            }
-
-        }
+        (holder as IViewHolder).bindView(
+            item = mItems[position],
+            isLastItem = position == mItems.size - 1
+        )
     }
 
-    private fun bindViewForViewHolderGridView(holder: RecyclerView.ViewHolder, item: Item) {
-        val viewHolder: ViewHolderGridView = holder as ViewHolderGridView
-        viewHolder.binding.productItemName.text = item.name
-        viewHolder.binding.productItemPrice.text = item.price
-        viewHolder.binding.productItemImage.clipToOutline = true
-        item.imageUrl?.let {
-            viewHolder.binding.productItemImage.load(Uri.parse(item.imageUrl)) {
-                placeholder(R.drawable.placeholder_broken_image)
-                error(R.drawable.placeholder_broken_image)
-            }
-            viewHolder.binding.productItemImage.setBackgroundResource(R.drawable.bg_colored_rounded_corner_shape_grid_item)
-        }
-    }
-
-    private fun bindViewForViewHolderListView(
-        holder: RecyclerView.ViewHolder,
-        item: Item,
-        isLastItem: Boolean
-    ) {
-        val viewHolder: ViewHolderListView = holder as ViewHolderListView
-
-        viewHolder.binding.productItemName.text = item.name
-        viewHolder.binding.productItemPrice.text = item.price
-        viewHolder.binding.productExtraInfo.text = item.extra
-
-        item.imageUrl?.let {
-            viewHolder.binding.productItemImage.load(Uri.parse(item.imageUrl)) {
-                placeholder(R.drawable.placeholder_broken_image)
-                error(R.drawable.placeholder_broken_image)
-            }
-            viewHolder.binding.productItemImage.setBackgroundResource(R.drawable.bg_colored_rounded_corner_shape_grid_item)
-        }
-
-        if (!isLastItem) {
-            viewHolder.binding.itemDivider.visibility = View.VISIBLE
-        } else {
-            viewHolder.binding.itemDivider.visibility = View.GONE
-        }
-    }
 }
