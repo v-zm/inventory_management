72e96754b0cc1d59a263e2be44d6aad98ec1c151
v-zm <vivek.yadav@zestmoney.in>
2023-03-06 14:13:10 +0530

    added api test

--- app/src/test/java/com/vivek/inventorymanagement/data/api/clients/InventoryHttpClientTest.kt
+++ app/src/test/java/com/vivek/inventorymanagement/data/api/clients/InventoryHttpClientTest.kt
@@ -1,7 +1,124 @@
 package com.vivek.inventorymanagement.data.api.clients
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.core.app.ApplicationProvider
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.vivek.inventorymanagement.data.api.ConnectivityAwareClient
+import com.vivek.inventorymanagement.data.api.dtos.ItemsDto
+import com.vivek.inventorymanagement.data.api.services.InventoryApiService
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import org.json.JSONObject
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TestRule
+import org.junit.runner.RunWith
+import org.mockito.MockitoAnnotations
+import org.robolectric.RobolectricTestRunner
+import java.net.HttpURLConnection
 
-class InventoryHttpClientTest{
+@RunWith(RobolectricTestRunner::class)
+class InventoryHttpClientTest {
+
+    @get:Rule
+    val taskInstantExecutor: TestRule = InstantTaskExecutorRule()
+
+    private lateinit var apiService: InventoryApiService
+    private lateinit var mockWebserver: MockWebServer
+    private val connectivityAwareClient: ConnectivityAwareClient =
+        ConnectivityAwareClient(ApplicationProvider.getApplicationContext())
+    private val client: IHttpClient = InventoryHttpClient(connectivityAwareClient)
+
+    @Before
+    fun setup() {
+        MockitoAnnotations.openMocks(this)
+        mockWebserver = MockWebServer()
+        mockWebserver.start()
+        apiService = client.getHttpClient().create(InventoryApiService::class.java)
+    }
+
+    @Test
+    fun fetchApiCall() {
+        val response = MockResponse()
+            .setResponseCode(HttpURLConnection.HTTP_OK)
+            .setBody(
+                "{\n" +
+                        "\t\"status\": \"success\",\n" +
+                        "\t\"error\": null,\n" +
+                        "\t\"data\": {\n" +
+                        "\t\t\"items\": [{\n" +
+                        "\t\t\t\"name\": \"Item 1\",\n" +
+                        "\t\t\t\"price\": \"₹ 100\",\n" +
+                        "                        \"image\": \"https://imgstatic.phonepe.com/images/dark/app-icons-ia-1/transfers/80/80/ic_check_balance.png\",\n" +
+                        "\t\t\t\"extra\": \"Same day shipping\"\n" +
+                        "\t\t}, {\n" +
+                        "\t\t\t\"name\": \"Item 2\",\n" +
+                        "            \t\t\"price\": \"₹ 400\",\n" +
+                        "            \t\t\"image\": \"https://imgstatic.phonepe.com/images/dark/app-icons-ia-1/transfers/80/80/ic_check_balance.png\",\n" +
+                        "\t\t\t\"extra\": \"Same day shipping\"\n" +
+                        "\t\t}, {\n" +
+                        "\t\t\t\"name\": \"Item 3\",\n" +
+                        "\t\t\t\"price\": \"₹ 100\",\n" +
+                        "            \t\t\"image\": \"https://imgstatic.phonepe.com/images/dark/app-icons-ia-1/transfers/80/80/ic_check_balance.png\",\n" +
+                        "\t\t\t\"extra\": \"Same day shipping\"\n" +
+                        "\t\t}, {\n" +
+                        "\t\t\t\"name\": \"Item 4\",\n" +
+                        "\t\t\t\"price\": \"₹ 80\",\n" +
+                        "            \t\t\"image\": \"https://imgstatic.phonepe.com/images/dark/app-icons-ia-1/transfers/80/80/ic_check_balance.png\",\n" +
+                        "\t\t\t\"extra\": null\n" +
+                        "\t\t}, {\n" +
+                        "\t\t\t\"name\": \"Item 5\",\n" +
+                        "\t\t\t\"price\": \"₹ 190\",\n" +
+                        "            \t\t\"image\": \"https://imgstatic.phonepe.com/images/dark/app-icons-ia-1/transfers/80/80/ic_check_balance.png\",\n" +
+                        "\t\t\t\"extra\": null\n" +
+                        "\t\t}, {\n" +
+                        "\t\t\t\"name\": \"Item 6\",\n" +
+                        "\t\t\t\"price\": \"₹ 70\",\n" +
+                        "            \t\t\"image\": \"https://imgstatic.phonepe.com/images/dark/app-icons-ia-1/transfers/80/80/ic_check_balance.png\",\n" +
+                        "\t\t\t\"extra\": null\n" +
+                        "\t\t}, {\n" +
+                        "\t\t\t\"name\": \"Item 7\",\n" +
+                        "\t\t\t\"price\": \"₹ 190\",\n" +
+                        "            \t\t\"image\": \"https://imgstatic.phonepe.com/images/dark/app-icons-ia-1/transfers/80/80/ic_check_balance.png\",\n" +
+                        "\t\t\t\"extra\": null\n" +
+                        "\t\t}, {\n" +
+                        "\t\t\t\"name\": \"Item 8\",\n" +
+                        "\t\t\t\"price\": \"₹ 190\",\n" +
+                        "            \t\t\"image\": \"https://imgstatic.phonepe.com/images/dark/app-icons-ia-1/transfers/80/80/ic_check_balance.png\",\n" +
+                        "\t\t\t\"extra\": null\n" +
+                        "\t\t}, {\n" +
+                        "\t\t\t\"name\": \"Item 9\",\n" +
+                        "\t\t\t\"price\": \"₹ 190\",\n" +
+                        "            \t\t\"image\": \"https://imgstatic.phonepe.com/images/dark/app-icons-ia-1/transfers/80/80/ic_check_balance.png\",\n" +
+                        "\t\t\t\"extra\": null\n" +
+                        "\t\t}]\n" +
+                        "\t}\n" +
+                        "}"
+            )
+        mockWebserver.enqueue(response)
+
+        val mockResponse = response.getBody()?.readUtf8()
+
+        val actualResponse = apiService.getInventoryList().execute()
+
+        assertEquals(
+            mockResponse?.let {
+                val a = parseMockedJson(it)
+                val mapper: ObjectMapper = ObjectMapper()
+                val item:
+                        ItemsDto = mapper.readValue(a, ItemsDto::class.java)
+                item
+            },
+            actualResponse.body()?.data
+        )
+    }
+
+    private fun parseMockedJson(mockResponse: String): String {
+        val reader = JSONObject(mockResponse)
+        return reader.getString("data")
+
+    }
 
 }
